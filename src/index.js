32f03592810c0b1d709675916eea3a9a7d2c6cfb
dverma-007 <deepak@moresimple.us>
2022-01-11 20:59:18 -0700

    Finished

--- src/index.js
+++ src/index.js
@@ -2,24 +2,27 @@ import { createResponseComposition } from 'msw';
 import './less/index.less'
 
 // Your code goes here!
-const firstNavButton = document.querySelector('nav a');
+const navButton = document.querySelectorAll('nav a');
 const h1LogoHeading = document.querySelector('h1');
 const introImg = document.querySelector('.intro img');
-const h2Intro = document.querySelector('.intro h2');
+const h2Intro = document.querySelectorAll('.intro h2');
 const pIntro = document.querySelector('.intro p');
-const pTextContent = document.querySelector('.text-content p');
+const pTextContent = document.querySelectorAll('.text-content p');
 const destination = document.querySelectorAll('.destination p');
 const h2ContentDestination = document.querySelector('h2');
 const pContentDestination = document.querySelector('p');
 const button = document.querySelectorAll('.btn');
 const footer = document.querySelector('.footer');
 const h4Header = document.querySelectorAll('h4');
+console.log(h4Header);
 
-firstNavButton.addEventListener('click', function(event) {
-    firstNavButton.style.color = "skyblue"
-    console.log("Looks like clicking on the Nav is working");
+for(let i=0; i<navButton.length; i++) {
+    navButton[i].addEventListener('click', function(event) {
+    navButton[i].style.color = "skyblue"
+    console.log("Looks like clicking on the Nav button is working by turning them into a blue font");
     console.log(event);
 })
+}
 
 h1LogoHeading.addEventListener('pointerover', function(evt) {
     h1LogoHeading.style["font-size"] = "8rem";
@@ -32,16 +35,18 @@ introImg.addEventListener('pointerover', function(evt) {
     console.log("We moved the pointer over the main image");
     console.log(`event type is ${evt}`);
 })
-pTextContent.addEventListener('pointerover', function(evt) {
-    // for(let i=0; i<pTextContent.length; i++) {
-    pTextContent.style.backgroundColor = "orange";
+for(let i=0; i<pTextContent.length; i++) {
+    pTextContent[i].addEventListener('pointerover', function(evt) {
+    pTextContent[i].style.backgroundColor = "yellow";
     console.log("The background color should change Orange for the p-tags");
     console.log(`event type: ${evt.type}`);
 })
-h4Header.addEventListener('click', function (evt) {
+}
+
     for(let i = 0; i<h4Header.length; i++) {
-    h4Header[i].style.color = "orange"
-    console.log("Clicking on h4 turns the font to orange");
-    console.log(`event Type: ${evt.type}`);
+        h4Header[i].addEventListener('click', function (evt) {
+        h4Header[i].style.color = "orange";
+        console.log("Clicking on h4 turns the font to orange");
+        console.log(`event Type: ${evt.type}`);
+    })
     }
-})
